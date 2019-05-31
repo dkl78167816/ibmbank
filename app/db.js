db2984fa9134cdc1eac748bd41bf5256bc762792
ddd <410491050@qq.com>
2019-05-31 14:29:44 +0800

    db fix 2

--- app/db.js
+++ app/db.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose"); //引入mongoose
-mongoose.connect('mongodb://localhost/list'); //连接到mongoDB的todo数据库
+mongoose.connect('mongodb://database/list'); //连接到mongoDB的todo数据库
+
 var ListSchema = new mongoose.Schema({
     user_id: String, //定义一个账户属性user_id，类型为String
     key: String, //定义一个密码属性key，类型为String
@@ -14,7 +15,6 @@ mongoose.model('user', ListSchema);
 var user=mongoose.model('user', ListSchema);
 var ins={user_id:"admin",key:"admin",account:100000,dingqi:0,foud:100000,stock:0};
 user.insertMany(ins,function(err,res){});
-console.log("2333333");
 var ins2={user_id:"user1",key:"user1",account:5000,dingqi:120,foud:50,stock:10000};
 user.insertMany(ins2,function(err,res){});
 
